534d3330e6d80fe99019864109a120134faf2612
Cristian Arboleda <cdarboleda@uce.edu.ec>
2022-06-21 19:50:39 -0500

    Unidad 2

---
+++ src/main/java/com/uce/edu/demo/ProyectoU2CaApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2CaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2CaApplication.class, args);
+	}
+
+}
