5f41558881d2916c1076110d71ee4e3d72d9ee2b
Cristian Arboleda <cdarboleda@uce.edu.ec>
2022-06-27 11:22:16 -0500

    Configuracion Log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2CaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2CaApplication.java
@@ -1,5 +1,7 @@
 package com.uce.edu.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,14 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoU2CaApplication implements CommandLineRunner{
 
+	private static final Logger logger = LogManager.getLogger(ProyectoU2CaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2CaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        //logger.debug("Debugging log");
+        logger.info("-------Hola Mundo------");
+        //logger.warn("Hey, This is a warning!");
+        //logger.error("Oops! We have an Error. OK");
+        //logger.fatal("Damn! Fatal error. Please fix me.");
+    }
 
 }
