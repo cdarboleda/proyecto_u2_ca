0433f35a33a461c64f4b0e5bfc813dc71e151592
Cristian Arboleda <cdarboleda@uce.edu.ec>
2022-07-26 20:28:26 -0500

    Ejercicio de Mapeos JPA

--- src/main/java/com/uce/edu/demo/ProyectoU2CaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2CaApplication.java
@@ -1,7 +1,6 @@
 package com.uce.edu.demo;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 //import org.apache.logging.log4j.LogManager;
@@ -12,12 +11,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.tarea24.repository.modelo.Autor2;
-import com.uce.edu.demo.tarea24.repository.modelo.Libro2;
-import com.uce.edu.demo.tarea24.repository.modelo.LibroAutor2;
+import com.uce.edu.demo.cajero.repository.modelo.Detalle;
+import com.uce.edu.demo.cajero.repository.modelo.Factura;
+import com.uce.edu.demo.cajero.service.IFacturaService;
 import com.uce.edu.demo.tarea24.service.IAutor2Service;
 import com.uce.edu.demo.tarea24.service.ILibro2Service;
-import com.uce.edu.demo.tarea24.service.ILibroAutor2Service;
 
 @SpringBootApplication
 public class ProyectoU2CaApplication implements CommandLineRunner{
@@ -33,7 +31,7 @@ public class ProyectoU2CaApplication implements CommandLineRunner{
 	private IAutor2Service autor2Service;
 	
 	@Autowired
-	private ILibroAutor2Service libroAutor2Service;
+	private IFacturaService facturaService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2CaApplication.class, args);
@@ -41,53 +39,16 @@ public class ProyectoU2CaApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-    	
-//    	Libro lib1 = new Libro();
-//    	lib1.setTitulo("Titulo 1");
-//    	lib1.setCantidad_paginas(200);
-//    	
-//    	Autor autor1 = new Autor();
-//    	autor1.setNombre("Cristian");
-//    	
-//    	Set<Autor> autores = new HashSet<>();
-//    	autores.add(autor1);
-//    	
-//    	lib1.setAutores(autores);
-//    	
-//    	this.libroService.insertar(lib1);
-    	
-    	Libro2 lib2 = new Libro2();
-    	lib2.setCantidad_paginas(200);
-    	lib2.setTitulo("ADios");
-    	this.libro2Service.insertar(lib2);
-    	
-    	Libro2 lib3 = new Libro2();
-    	lib3.setCantidad_paginas(100);
-    	lib3.setTitulo("Chao");
-    	this.libro2Service.insertar(lib3);
-    	
-    	Autor2 autor2 = new Autor2();
-    	autor2.setNombre("David");
-    	this.autor2Service.insertar(autor2);
-    	
-    	LibroAutor2 libroAutor2 = new LibroAutor2();
-    	lib2.setId(2);
-    	autor2.setId(1);
-    	libroAutor2.setAutor2(autor2);
-    	libroAutor2.setLibro2(lib2);
-    	
-    	LibroAutor2 libroAutor3 = new LibroAutor2();
-    	lib3.setId(3);
-    	autor2.setId(2);
-    	libroAutor3.setAutor2(autor2);
-    	libroAutor3.setLibro2(lib3);
-    	
-    	List<LibroAutor2> listaRompimiento = new ArrayList<>();
-    	listaRompimiento.add(libroAutor2);
-    	listaRompimiento.add(libroAutor3);
-    	
-    	this.libroAutor2Service.insertar(libroAutor2);
-    	this.libroAutor2Service.insertar(libroAutor3);
+    	Factura fact=this.facturaService.consultar(1);
+
+        logger.info("Numero: "+fact.getNumero());
+        logger.info("fecha: "+fact.getFecha());
+        logger.info("cliente: "+fact.getCliente().getNumeroTarjeta());
+
+        List<Detalle> detalles = fact.getDetalles();
+        for(Detalle deta:detalles) {
+            logger.info("detalle: Cantidad: "+deta.getCantidad()+" Subtotal: "+deta.getSubtotal());
+        }
     	
     }
 
--- src/main/java/com/uce/edu/demo/cajero/repository/modelo/Cliente.java
+++ src/main/java/com/uce/edu/demo/cajero/repository/modelo/Cliente.java
@@ -1,42 +1,44 @@
-package com.uce.edu.demo.tarea23.repository.modelo;
+package com.uce.edu.demo.cajero.repository.modelo;
 
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-@Entity
+@Entity(name = "Cliente2")
 @Table(name = "cliente")
 public class Cliente {
 	@Id
+	@Column(name = "clie_id")
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "clie_id_seq")
 	@SequenceGenerator(name = "clie_id_seq", sequenceName="clie_id_seq", allocationSize =1)
-	@Column(name = "clie_id")
 	private Integer id;
 	
-	@Column(name = "clie_nombre")
-	private String nombre;
+	@Column(name = "clie_numero_tarjeta")
+	private String numeroTarjeta;
 	
-	@Column(name = "clie_cedula")
-	private String cedula;
+	@OneToOne
+	@JoinColumn(name = "clie_ciud_id")
+	private Ciudadano ciudadano;
 	
-	@OneToMany(mappedBy = "cliente", fetch=FetchType.EAGER)
+	@OneToMany(mappedBy="cliente")
 	private List<Factura> facturas;
-
 
 	@Override
 	public String toString() {
-		return "Cliente [id=" + id + ", nombre=" + nombre + ", cedula=" + cedula + ", facturas=" + facturas + "]";
+		return "Cliente [id=" + id + ", numeroTarjeta=" + numeroTarjeta + ", ciudadano=" + ciudadano + ", facturas="
+				+ facturas + "]";
 	}
 
-	//Seter y Geter
+	//SET Y GET
 	public Integer getId() {
 		return id;
 	}
@@ -45,20 +47,20 @@ public class Cliente {
 		this.id = id;
 	}
 
-	public String getNombre() {
-		return nombre;
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
 	}
 
-	public String getCedula() {
-		return cedula;
+	public Ciudadano getCiudadano() {
+		return ciudadano;
 	}
 
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
+	public void setCiudadano(Ciudadano ciudadano) {
+		this.ciudadano = ciudadano;
 	}
 
 	public List<Factura> getFacturas() {
